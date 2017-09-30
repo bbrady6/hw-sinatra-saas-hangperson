095ea29732a81bf82e79741e1020f0be8c7b865c
Benjamin Brady <bbrady6@uncc.edu>
2017-09-30 15:48:05 +0000

    Set up the Gemfile

--- app.rb
+++ app.rb
@@ -39,7 +39,13 @@ class HangpersonApp < Sinatra::Base
   # If a guess is invalid, set flash[:message] to "Invalid guess."
   post '/guess' do
     letter = params[:guess].to_s[0]
-    ### YOUR CODE HERE ###
+    begin
+      if !@gami.guess(letter)
+        flash[:message]
+      end
+    rescue AugumentError
+      flash[:message] = "Invalid Guess"
+    end
     redirect '/show'
   end
   
@@ -49,17 +55,28 @@ class HangpersonApp < Sinatra::Base
   # Notice that the show.erb template expects to use the instance variables
   # wrong_guesses and word_with_guesses from @game.
   get '/show' do
-    ### YOUR CODE HERE ###
+    res = @game.check_win_or_lose
+    if res == :win
+      redirect '/win'
+    elsif res == :lose
+      redirect '/lose'
+    end
     erb :show # You may change/remove this line
   end
   
   get '/win' do
-    ### YOUR CODE HERE ###
+    if @game.check_win_or_lose == :play
+      redirect '/show'
+    end
+    
     erb :win # You may change/remove this line
   end
   
   get '/lose' do
-    ### YOUR CODE HERE ###
+    if @game.check_win_or_lose == :play
+      redirect '/show'
+    end
+    
     erb :lose # You may change/remove this line
   end
   
